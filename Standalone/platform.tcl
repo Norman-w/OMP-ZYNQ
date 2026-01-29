99bf9ba9309f7c3ae42b551045aca58c09d4d47c
Norman-W <85535885@qq.com>
2026-01-29 16:55:19 +0800

    🔧 更新.gitignore并添加PS端Standalone平台项目
    使用小梅哥的xsa文件生成的standalone平台项目。
    已配置忽略Vitis工作空间临时文件和编译产物。

---
+++ Standalone/platform.tcl
@@ -0,0 +1,18 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct D:\ZYNQ\Norman\OMP\Standalone\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source D:\ZYNQ\Norman\OMP\Standalone\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {Standalone}\
+-hw {D:\ZYNQ\Norman\OMP\PL\System_wrapper.xsa}\
+-proc {ps7_cortexa9_0} -os {standalone} -out {D:/ZYNQ/Norman/OMP}
+
+platform write
+platform generate -domains 
+platform active {Standalone}
+platform generate
